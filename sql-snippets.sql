139852dd1dfaaf47ba06aef61207f6ea462bffab
Anna J <anna.jones@novicap.com>
2016-09-07 15:23:42 +0200

    Update sql-snippets.sql

--- sql-snippets.sql
+++ sql-snippets.sql
@@ -4,3 +4,7 @@ SELECT *
  WHERE created_at::date >= date_trunc('MONTH', current_date - INTERVAL '1 MONTH' )::DATE
    AND created_at::date <= (date_trunc('MONTH', current_date - INTERVAL '1 MONTH' ) + INTERVAL '1 MONTH - 1 day')::DATE;
 
+-- list all the tables with a name that matches a pattern e.g. table_*, ignoring case
+SELECT *
+  FROM information_schema.tables 
+ WHERE table_name ILIKE '%table%'
