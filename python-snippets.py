7a0d4d8ce3261710458a5ded91a68b22fc75fa4d
Anna J <anna.jones@novicap.com>
2016-09-13 10:38:16 +0200

    Update python-snippets.py

--- python-snippets.py
+++ python-snippets.py
@@ -7,3 +7,7 @@ if not os.path.isfile('filename.csv'):
     else:  
         # else the file exists so ignore the header
         df.to_csv('filename.csv', mode='a', header=False, encoding='utf-8')
+
+# save dataframe in a pickle that's compatible with python 2
+pickle.dump(df , open( "dataframe_pickle.p", "wb" ) , protocol = 2)
+df = pickle.load( open( "dataframe_pickle.p", "rb" ))
