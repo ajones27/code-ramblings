907fac9ea92522718341205cda291de7ac58ea03
Anna J <anna.jones@novicap.com>
2016-11-11 12:24:34 +0100

    Update ruby_snippets.rb

--- ruby_snippets.rb
+++ ruby_snippets.rb
@@ -5,3 +5,11 @@ entity_to_count.each do |my_entity|
   # do something with them
   bar.increment!
 end
+
+# convert two arrays (one of keys, one of values) into a hash
+my_hash = Hash[keys_array.zip(values_array)] 
+
+# convert array of strings to array of symbols
+string_array = ["My Name", "My age", "My new ADDRESS "]
+symbol_array = string_array.map {|elem| elem.downcase.strip.parameterize.underscore.to_sym}
+# => [:my_name, :my_age, :my_new_address]
