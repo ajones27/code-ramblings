ae04b5388076012e204393504d5857e293066ece
Anna J <anna.jones@novicap.com>
2016-11-18 10:56:41 +0100

    Update ruby_snippets.rb

--- ruby_snippets.rb
+++ ruby_snippets.rb
@@ -14,3 +14,17 @@ string_array = ["My Name", "My age", "My new ADDRESS "]
 symbol_array = string_array.map {|elem| elem.downcase.strip.parameterize.underscore.to_sym}
 # => [:my_name, :my_age, :my_new_address]
 
+# Parse an excel sheet and select a specific sheet
+excel = Roo::Spreadsheet.open(File.join(Rails.root, "file_name.xlsx"))
+my_first_sheet = JSON.parse(excel.sheet(0).to_json)
+my_second_sheet = JSON.parse(excel.sheet(1).to_json)
+
+# Parse a csv with latin-1 (iso-8859-1) encoding and different separator (';')
+my_csv = CSV.read(File.join(Rails.root, 'file_name.csv'), headers: true, col_sep: ';', encoding: "iso-8859-1:utf-8")
+headers = my_csv.headers
+first_row = my_csv.first
+
+# Edit a csv row by row
+my_csv.map do |row|
+  row["New column"] = row["Old column"] - 1
+end
